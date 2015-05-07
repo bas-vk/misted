ccb8f77fcc1de439ab2ad895dd1813b908c0490f
Bas van Kervel <basvankervel@ziggo.nl>
2015-05-07 17:20:01 +0200

    integrated logging

--- js/mist.js
+++ js/mist.js
@@ -1,13 +1,48 @@
 $(function() {
 
 	var accessToken = "";
+	var accessTokenLength = 16;
 	var gethLog = [];
 	var maxGethLog = 10;
+	var eventLog = [];
+	var maxEventLog = 10;
+	var gethWebSocket = null;
+	// number of retries to connect to geth before giving up
+	var maxRetries = 3;
 
 	var crypto = require('crypto');
 
 	function newAccessToken(len) {
-		return crypto.randomBytes(Math.ceil(16)).toString('hex'); // convert to hexadecimal format
+		return crypto.randomBytes(Math.ceil(accessTokenLength)).toString('hex'); // convert to hexadecimal format
+	}
+
+	function handleEvent(event) {
+		eventLog.push(event);
+		if (eventLog.length > maxEventLog) {
+			eventLog.shift();
+		}
+
+		var events = "";
+		eventLog.reverse().forEach(function(ev) {
+			events += JSON.stringify(ev) + "<br />";
+		});
+
+		$("#eventlog").html(events);
+	}
+
+	function handleHashrate(reply) {
+		$("#hashrate").val(reply.result.hashrate);
+	}
+
+	function handleMessage(message) {
+		var response = JSON.parse(message);
+		if (response.method == "event") {
+			handleEvent(response);
+		} else if (response.method == "miner_hashrate") {
+			handleHashrate(response);
+		} else {
+			console.log("unhandled message: " + JSON.stringify(response))
+		}
 	}
 
 	function updateGethLog(line) {
@@ -24,6 +59,33 @@ $(function() {
 		$("#gethlog").html(logs);
 	}
 
+	function openGethWebSocket() {
+		if (maxRetries == 0) {
+			return;
+		}
+
+		var WebSocket = require("ws");
+		gethWebSocket = new WebSocket("ws://localhost:8546/geth?accessToken=" + accessToken);
+
+		gethWebSocket.on("message", function(message) {
+			handleMessage(message);
+		});
+
+		gethWebSocket.on("close", function() {
+			$("#wsstatus").html("disconnected");
+			maxRetries -= 1;
+			openGethWebSocket();
+		});
+
+		gethWebSocket.on("error", function() {
+			$("#wsstatus").html("error");
+		});
+
+		gethWebSocket.on("open", function() {
+			$("#wsstatus").html("connected");
+		});
+	};
+
 	$("#startGethBtn").click(function() {
 		var spawn = require('child_process').spawn;
 		environ = {};
@@ -31,10 +93,13 @@ $(function() {
 			environ[v] = process.env[v];
 		}
 		
-		accessToken = newAccessToken();
+		if (accessToken.length != accessTokenLength) {
+			accessToken = newAccessToken();
+		}
+
 		environ["accessToken"] = accessToken;
 
-		geth = spawn("geth", ["--rpcport=8686", "--port=31313", "--datadir=$HOME/tmp/ethereum1", "mist"], {env: environ});
+		geth = spawn("geth", ["--rpccorsdomain=http://localhost:3000", "mist"], {env: environ});
 
 		geth.stderr.on("data", function(line) {
 			updateGethLog(line);
@@ -45,48 +110,44 @@ $(function() {
 		});
 	});
 
-	$("#stopGethBtn").click(function() {
-		var WebSocket = require("ws");
-		var geth = new WebSocket("ws://localhost:8546/geth?accessToken=" + accessToken);
-
-		geth.on("open", function() {
-			geth.send('{"jsonrpc":"0.0.1","method":"quit","params":[],"id":1}');
-		});
-
-		geth.on("message", function(message) {
-			console.log("message recv: ", message)
-			geth.close();
-		});
+	$("#openGethWebsocket").click(function() {
+		maxRetries = 3;
+		openGethWebSocket();
 	});
 
+	$("#closeGethWebsocket").click(function() {
+		if (gethWebSocket != null) {
+			gethWebSocket.close();
+		}
+	});	
+		
+	$("#toggleLogBtn").click(function() {
+		$(".logPart").toggle();
+	});
+
+	$("#toggleEventsBtn").click(function() {
+		$(".gethEvents").toggle();
+	});
+
+	$("#stopGethBtn").click(function() {
+		gethWebSocket.send('{"jsonws":"0.0.1","method":"quit","params":[],"id":1}');
+		geth.close();
+	});
+
+	$("#loadWalletDappBtn").click(function() {
+		$("#dapp-wallet").attr("src", "http://localhost:3000");
+	});
+	
 	$("#startMinerBtn").click(function() {
-		var WebSocket = require("ws");
-		var geth = new WebSocket("ws://localhost:8546/geth?accessToken=" + accessToken);
-
-		geth.on("open", function() {
-			geth.send('{"jsonrpc":"0.0.1","method":"miner_start","params":[],"id":1}');
-		});
-
-		geth.on("message", function(message) {
-			console.log("message recv: ", message)
-			geth.close();
-		});
+		gethWebSocket.send('{"jsonrpc":"0.0.1","method":"miner_start","params":[],"id":1}');
 	});
 
 	$("#stopMinerBtn").click(function() {
-		var WebSocket = require("ws");
-		var geth = new WebSocket("ws://localhost:8546/geth?accessToken=" + accessToken);
-
-		geth.on("open", function() {
-			geth.send('{"jsonrpc":"0.0.1","method":"miner_stop","params":[],"id":1}');
-		});
-
-		geth.on("message", function(message) {
-			console.log("message recv: ", message)
-			geth.close();
-		});
+		gethWebSocket.send('{"jsonrpc":"0.0.1","method":"miner_stop","params":[],"id":1}');
 	});
 
+	$("#getHashrateBtn").click(function() {
+		gethWebSocket.send('{"jsonws":"0.0.1","method":"miner_hashrate","params":[],"id":1}');
+	});
 
-	
 });
